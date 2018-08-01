4252ed953a3f4141aa9918e2483d059df1833f89
agriks <kdstratton@gmail.com>
2018-07-31 22:54:12 -0400

    Added an initial batch of instructions

--- src/cpu.c
+++ src/cpu.c
@@ -3,12 +3,32 @@
 #include <malloc.h>
 #include <memory.h>
 
+#include <time.h>
+
+/*
+ * Registers:
+ * A F (Z, N, H, C flag bits)
+ * B C
+ * D E
+ * H L
+ * SP
+ * PC
+ */
 struct cpu_state
 {
 	union {
 		uint16_t af;
 		struct {
-			uint8_t f;
+			union {
+				uint8_t f;
+				struct {
+					uint8_t fl : 4;
+					uint8_t fc : 1; // (C) Carry
+					uint8_t fh : 1; // (H) Half Carry
+					uint8_t fn : 1; // (N) Subtract
+					uint8_t fz : 1; // (Z) Zero
+				};
+			};
 			uint8_t a;
 		};
 	};
@@ -39,12 +59,244 @@ struct cpu_state
 
 };
 
-void load8(uint8_t *dest, uint8_t src) {
-	memcpy(dest, &src, 1);
+uint8_t rot_left8(uint8_t val) {
+	return val << 1 | val >> 7;
+
 }
 
-void print_registers(struct cpu_state *s) {
-	printf("A: %02X, B: %02X, C: %02X, D: %02X, E: %02X, F: %02X, H: %02X, L: %02X, SP: %04X, PC: %04X\n", s->a, s->b, s->c, s->d, s->e, s->f, s->h, s->l, s->sp, s->pc);
+uint8_t rot_right8(uint8_t val) {
+	return val >> 1 | val << 7;
+
+}
+
+void set_left_shift_flags(struct cpu_state *state, uint8_t val, uint8_t old7bit) {
+	state->fn = 0;
+	state->fh = 0;
+	if (val == 0) {
+		state->fz = 1;
+	}
+	state->fc = old7bit;
+
+}
+
+void set_right_shift_flags(struct cpu_state *state, uint8_t val, uint8_t old0bit) {
+	state->fn = 0;
+	state->fh = 0;
+	if (val == 0) {
+		state->fz = 1;
+	}
+	state->fc = old0bit;
+
+}
+
+void set_add16_flags(struct cpu_state *state, uint16_t a, uint16_t b) {
+	state->fn = 0;
+	state->fh = ((a & 0xfff) + (b & 0xfff)) & 0x100;
+	state->fc = ((uint32_t)a + (uint32_t)b) & 0x1000;
+}
+
+void set_add8_flags(struct cpu_state *state, uint8_t a, uint8_t b, int use_carry) {
+	state->fn = 0;
+	if (a + b == 0) {
+		state->fz = 1;
+	}
+	state->fh = ((a & 0x0f) + (b & 0x0f)) & 0x10;
+	if (use_carry) {
+		state->fc = (((uint16_t)a & 0xff) + ((uint16_t)b & 0xff)) & 0x100;
+	}
+}
+
+void set_dec_flags(struct cpu_state *state, uint8_t val) {
+	state->fn = 1;
+	if (val == 0) {
+		state->fz = 1;
+	}
+	if ((val + 1 >> 4) == 0x00) {
+		state->fz = 1;
+	}
+
+}
+
+int execute_cb(struct cpu_state *state) {
+	int pc = state->pc;
+	uint8_t *op = &state->mem[pc];
+	int cycles = 0;
+	switch (*op) {
+		case 0x7C:
+			state->fz = state->h | 0x80;
+			state->fn = 0;
+			state->fh = 1;
+			state->pc++;
+			cycles = 8;
+			break;
+		default:
+			printf("CB %02X instruction not implemented yet", state->mem[pc]);
+			break;
+	}
+	return cycles;
+}
+
+/*
+ * Executes operation in memory at PC. Updates PC reference.
+ * Returns number of clock cycles.
+ */
+int execute(struct cpu_state *state) {
+	int pc = state->pc;
+	uint8_t *op = &state->mem[pc];
+	int cycles = 0;
+	state->pc++;
+	switch (*op) {
+		case 0x00:
+			/* NOP */
+			cycles = 4;
+			break;
+		case 0x01:
+			/* LD BC,nn */
+			state->c = op[1];
+			state->b = op[2];
+			cycles = 12;
+			state->pc += 2;
+			break;
+		case 0x02:
+			/* LD (BC),A */
+			state->mem[state->bc] = state->a;
+			cycles = 8;
+			break;
+		case 0x03:
+			/* INC BC */
+			state->bc++;
+			cycles = 8;
+			break;
+		case 0x04:
+			/* INC B */
+			set_add8_flags(state, state->b, 1, 0);
+			state->b++;
+			cycles = 4;
+			break;
+		case 0x05:
+			/* DEC B */
+			set_dec_flags(state, state->b--);
+			cycles = 4;
+			break;
+		case 0x06:
+			/* LD B,n */
+			state->b = op[1];
+			state->pc++;
+			cycles = 8;
+			break;
+		case 0x07:
+			/* RLCA */
+			state->a = rot_left8(state->a);
+			set_left_shift_flags(state, state->a, state->a >> 7);
+			cycles = 4;
+			break;
+		case 0x08:
+			/* LD (nn),SP */
+			uint16_t addr = (op[2] << 8) | op[1];
+			state->mem[addr] = state->sp;
+			state->pc += 2;
+			cycles = 20;
+			break;
+		case 0x09:
+			/* ADD HL,BC */
+			set_add16_flags(state, state->hl, state->bc);
+			state->hl += state->bc;
+			cycles = 8;
+			break;
+		case 0x0A:
+			/* LD A,(BC) */
+			state->a = state->mem[state->bc];
+			cycles = 8;
+			break;
+		case 0x0B:
+			/* DEC BC */
+			state->bc--;
+			cycles = 8;
+			break;
+		case 0x0C:
+			/* INC C */
+			set_add8_flags(state, state->c, 1, 0);
+			state->c++;
+			cycles = 4;
+			break;
+		case 0x0D:
+			/* DEC C */
+			set_dec_flags(state, state->c--);
+			cycles = 4;
+			break;
+		case 0x0E:
+			/* LD C,n */
+			state->c = op[1];
+			state->pc++;
+			cycles = 8;
+			break;
+		case 0x0F:
+			/* RRCA */
+			state->fn = 0;
+			state->fh = 0;
+			state->fc = state->a & 0x01;
+			state->a = rot_right8(state->a);
+			if (state->a == 0) {
+				state->fz = 1;
+			}
+			cycles = 4;
+			break;
+		case 0x10:
+			/* STOP 0 */
+			puts("STOP 0 (0x1000) not implemented\n");
+			state->pc++;
+			cycles = 4;
+		case 0x20:
+			// LEFT OFF HERE
+			int8_t offset = op[1];
+			state->pc++;
+			if (state->fz == 0) {
+				state->pc += offset;
+			}
+			break;
+		case 0x21:
+			/* LD HL,nn */
+			state->l = op[1];
+			state->h = op[2];
+			cycles = 12;
+			state->pc += 2;
+			break;
+		case 0x31:
+			/* LD SP,nn */
+			uint16_t nn = (op[2] << 8) | op[1];
+			state->sp = nn;
+			cycles = 12;
+			state->pc += 2;
+			break;
+		case 0x32:
+			/* LD (HL-),A */
+			state->hl = state->a;
+			state->hl--;
+			cycles = 8;
+			break;
+		case 0xAF:
+			/* XOR A */
+			state->a = 0;
+			state->fz = 1;
+			state->fh = 0;
+			state->fn = 0;
+			state->fc = 0;
+			break;
+		case 0xCB:
+			cycles = execute_cb(state);
+			break;
+		default:
+			printf("Not implemented yet");
+			break;
+
+	};
+	return cycles;
+}
+
+
+void print_registers(struct cpu_state *state) {
+	printf("A: %02X, B: %02X, C: %02X, D: %02X, E: %02X, F: %02X, H: %02X, L: %02X, SP: %04X, PC: %04X\n", state->a, state->b, state->c, state->d, state->e, state->f, state->h, state->l, state->sp, state->pc);
+	printf("FLAGS: Z:%d N:%d H:%d C:%d None:%d\n", state->fz, state->fn, state->fh, state->fc, state->fl );
 }
 
 void print_memory(struct cpu_state *s) {
@@ -65,7 +317,7 @@ void power_up(struct cpu_state *state) {
 	// least sig bit eq to zero
 	// TODO: disable internal ROM and begin cartridge exec at $100
 	
-	// potentiall wrong find other docs
+	// potentially wrong find other docs
 	state->a = 0x01;
 	state->f = 0xB0;
 	state->b = 0x00;
@@ -113,7 +365,6 @@ void start() {
 	state->mem = calloc(0x10000, sizeof(uint8_t));
 	print_registers(state);
 	power_up(state);
-	state->af = 0x10BB;
 	print_registers(state);
 }
 
